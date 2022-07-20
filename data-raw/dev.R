54a008b9f7af4c79803833376aa1660a41d9a3f4
hypnicjerk91 <eleni.gvasilaki@gmail.com>
2022-07-20 11:48:04 +0300

    create desc

--- data-raw/dev.R
+++ data-raw/dev.R
@@ -6,7 +6,32 @@ library(desc)
 library(roxygen2)
 library(qpdf)
 
+# Remove default DESC
+unlink("DESCRIPTION")
+# Create and clean desc
+my_desc <- description$new("!new")
 
+# Set your package name
+my_desc$set("Package", "datagovgR")
+
+#Set your name
+my_desc$set("Authors@R", "person('Eleni', 'Vasilaki', email = 'eleni.gvasilaki@gmail.com', role = c('cre', 'aut'))")
+
+# Remove some author fields
+my_desc$del("Maintainer")
+
+# Set the version
+my_desc$set_version("0.0.1")
+
+# The title of your package
+my_desc$set(Title = "R wrapper for data.gov.gr API")
+# The description of your package
+my_desc$set(Description = "The goal of datagovgR is to enable R users, data analysts, academics and students to work with the Greek open governmental data programmatically using their favourite language, R.")
+# The urls
+my_desc$set("URL", "https://github.com/hypnicjerk91/datagovgR")
+my_desc$set("BugReports", "https://github.com/hypnicjerk91/datagovgR")
+# Save everyting
+my_desc$write(file = "DESCRIPTION")
 
 
 # Use this
@@ -18,8 +43,6 @@ usethis::use_package("jsonlite")
 usethis::use_package("curl")
 usethis::use_package("attempt")
 
-
-
 use_testthat()
 use_vignette("datagovgR")
-use_readme_rmd()
+
